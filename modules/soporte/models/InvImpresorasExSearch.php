644df3798654b29b3e9e2f977dc8b95ccc6c0353
Cesar Morones <cesar.morones1@gmail.com>
2017-07-03 14:01:16 -0500

    cambio en impresora

--- modules/soporte/models/InvImpresorasExSearch.php
+++ modules/soporte/models/InvImpresorasExSearch.php
@@ -57,6 +57,9 @@ class InvImpresorasExSearch extends InvImpresorasEx
             return $dataProvider;
         }
 
+          $id_p = Yii::$app->user->identity->id_plantel;
+
+
         // grid filtering conditions
         $query->andFilterWhere([
             'id' => $this->id,
@@ -65,7 +68,7 @@ class InvImpresorasExSearch extends InvImpresorasEx
             'marca' => $this->marca,
             'modelo' => $this->modelo,
             'estado' => $this->estado,
-            'id_plantel' => $this->id_plantel,
+            'id_plantel' =>  $id_p,
             'id_area' => $this->id_area,
             'id_piso' => $this->id_piso,
             'antiguedad' => $this->antiguedad,
