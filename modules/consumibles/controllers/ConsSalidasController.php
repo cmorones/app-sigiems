f2ef1444db5c03db7aa6f784dfd796ac1f598680
Cesar Morones <cesar.morones1@gmail.com>
2018-06-19 18:25:15 -0500

    Agregar Documento Refacciones4

--- modules/consumibles/controllers/ConsSalidasController.php
+++ modules/consumibles/controllers/ConsSalidasController.php
@@ -35,7 +35,7 @@ class ConsSalidasController extends Controller
                // 'only' => ['logout'],
                 'rules' => [
                     [
-                        'actions' => ['index','create','view','update','docto','pdf'],
+                        'actions' => ['index','create','view','update','docto','pdf', 'salidas'],
                         'allow' => true,
                         'roles' => ['@'],
                     ],
