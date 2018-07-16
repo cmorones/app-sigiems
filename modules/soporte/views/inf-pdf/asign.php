89841e8912a8142ada1ec3158b37ff186335104d
Cesar Morones <cesar.morones1@gmail.com>
2018-07-16 00:22:40 -0500

    asign pdf2

--- modules/soporte/views/inf-pdf/asign.php
+++ modules/soporte/views/inf-pdf/asign.php
@@ -41,7 +41,7 @@ FROM
   public.personal,
   public.situacion_bienes
 WHERE
-  (bienes_muebles.clave_cabms = $clabe_cabs and 
+  (bienes_muebles.clave_cabms ='".$clabe_cabs."'" and 
   bienes_muebles.progresivo = $model->progresivo and
   bienes_muebles.id_situacion_bien = situacion_bienes.id_situacion_bien and  
   resguardos.id_bien_mueble = bienes_muebles.id_bien_mueble AND
